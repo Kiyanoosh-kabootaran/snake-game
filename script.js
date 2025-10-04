079dfb93f66e8e66e2a72eda5d18c6b88d153560
Kiyanoosh Kabootaran <kkabootaran@gmail.com>
2025-10-04 11:31:55 +0330

    change food position randomly

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const playBoard = document.querySelector('.play-board');
 
-let foodX = 13 , foodY = 10;
+let foodX , foodY;
+
+const changeFoodPosition = () => {
+  //Passing a random 0-30 value as food position
+  foodX = Math.floor(Math.random() * 30) + 1;
+  foodY = Math.floor(Math.random() * 30) + 1;
+}
 
 const initGame = () => {
   const htmlMarkup = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
   playBoard.innerHTML = htmlMarkup;
 }
 
+changeFoodPosition();
 initGame();
