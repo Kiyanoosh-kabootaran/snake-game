6bcea9e2abdd84cf0ae8330cd8d13a3d1cfa5ee1
Kiyanoosh Kabootaran <kkabootaran@gmail.com>
2025-10-04 12:26:32 +0330

    make head moving by key press

--- script.js
+++ script.js
@@ -1,17 +1,44 @@
 const playBoard = document.querySelector('.play-board');
 
 let foodX , foodY;
-
+let snakeX = 5 , snakeY = 10;
+let velocityX = 0 ,velocityY = 0;
 const changeFoodPosition = () => {
   //Passing a random 0-30 value as food position
   foodX = Math.floor(Math.random() * 30) + 1;
   foodY = Math.floor(Math.random() * 30) + 1;
 }
 
+const changeDirection = (e) => {
+  if(e.key === "ArrowUp"){
+    velocityY = -1;
+    velocityX = 0;
+
+  } else if(e.key === "ArrowDown"){
+    velocityY = 1;
+    velocityX = 0;
+
+  } else if(e.key === "ArrowLeft"){
+    velocityY = 0;
+    velocityX = -1;
+
+  } else if(e.key === "ArrowRight"){
+    velocityY = 0;
+    velocityX = 1;
+  }
+}
+
 const initGame = () => {
-  const htmlMarkup = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
+  snakeX += velocityX;
+  snakeY += velocityY;
+
+  let htmlMarkup = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
+  htmlMarkup += `<div class="head" style="grid-area: ${snakeY} / ${snakeX}"></div>`;
   playBoard.innerHTML = htmlMarkup;
 }
 
 changeFoodPosition();
 initGame();
+
+setInterval(initGame , 125);
+document.addEventListener("keydown" , changeDirection);
